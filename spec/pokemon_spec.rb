794ffbafa0078e89c0ba3d69dc2b54312852af15
Tammy Metz <tammy@tammymetz.com>
2018-09-22 09:52:36 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -4,6 +4,7 @@ describe "Pokemon" do
   before do
     @db = SQLite3::Database.new(':memory:')
     @db.execute("DROP TABLE IF EXISTS pokemon")
+    @sql_runner.execute_create_hp_column
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
   end
@@ -43,7 +44,7 @@ describe "Pokemon" do
   describe "BONUS" do
 
     before do
-      @sql_runner.execute_create_hp_column
+      #@sql_runner.execute_create_hp_column
       Pokemon.save('Pikachu', 'electric', @db)
       Pokemon.save('Magikarp', 'water', @db)
     end
