b5d361977acecaead3991992d89712cac05980c5
Tammy Metz <tammy@tammymetz.com>
2018-09-22 09:53:51 +0000

    automatically backed up by learn

--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -6,7 +6,6 @@ describe "Pokemon" do
     @db.execute("DROP TABLE IF EXISTS pokemon")
     @sql_runner = SQLRunner.new(@db)
     @sql_runner.execute_schema_migration_sql
-    @sql_runner.execute_create_hp_column
   end
 
   let(:pokemon) {Pokemon.new(id: 1, name: "Pikachu", type: "electric", db: @db)}
@@ -44,7 +43,7 @@ describe "Pokemon" do
   describe "BONUS" do
 
     before do
-      #@sql_runner.execute_create_hp_column
+      @sql_runner.execute_create_hp_column
       Pokemon.save('Pikachu', 'electric', @db)
       Pokemon.save('Magikarp', 'water', @db)
     end
